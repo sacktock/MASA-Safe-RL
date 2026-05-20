fc2747caaabb58b4c8acd6006168d87a90c1a4ec
nightly <47933317+nightly@users.noreply.github.com>
2026-05-20 23:17:47 +0100

    Fix continuous MountainCar render FPS

--- masa/envs/continuous/mountain_car.py
+++ masa/envs/continuous/mountain_car.py
@@ -25,7 +25,7 @@ def label_fn(obs):
 cost_fn = lambda labels: 1.0 if "wall" in labels else 0.0
 
 class ContinuousMountainCar(ContinuousEnv):
-    metadata = {"render_modes": ["ansi", "rgb_array", "human"], "render_fps": 20}
+    metadata = {"render_modes": ["ansi", "rgb_array", "human"], "render_fps": 30}
 
     def __init__(
         self,
